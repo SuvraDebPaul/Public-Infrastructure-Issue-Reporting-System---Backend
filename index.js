f3507cf0292f574f0389c1b4de90d31765d6a3bc
Suvra Deb Paul <suvra.deb.paul@gmail.com>
2025-12-10 22:43:44 +0600

    Working on Issue Details Page Api

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const uri = process.env.MONGO_URI;
 const port = process.env.PORT || 3000;
@@ -19,7 +20,7 @@ const client = new MongoClient(uri, {
 //Middlewares
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [process.env.CLIENT_DOMAIN],
     credentials: true,
     optionsSuccessStatus: 200,
   })
@@ -30,11 +31,58 @@ async function run() {
   try {
     const db = client.db("PIIRS");
     const IssuesCollection = db.collection("Issues");
+    const PaymentsCollection = db.collection("Payments");
 
     app.get("/issues", async (req, res) => {
       const result = await IssuesCollection.find().toArray();
       res.send(result);
     });
+    //Getting a Single Issue
+    app.get("/issues/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await IssuesCollection.findOne(query);
+      // console.log(result);
+      res.send(result);
+    });
+    //Updating a Single Issue
+    app.put("/issues/:id", async (req, res) => {
+      const id = req.params.id;
+      const updatedData = req.body;
+      const result = await IssuesCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: updatedData }
+      );
+      res.send(result);
+    });
+    // Deleting a Single Issue
+    // Delete an issue by ID
+    app.delete("/issues/:id", async (req, res) => {
+      const id = req.params.id;
+      try {
+        const result = await IssuesCollection.deleteOne({
+          _id: new ObjectId(id),
+        });
+
+        if (result.deletedCount === 1) {
+          res.send({ success: true, message: "Issue deleted successfully" });
+        } else {
+          res.status(404).send({ success: false, message: "Issue not found" });
+        }
+      } catch (error) {
+        console.error(error);
+        res
+          .status(500)
+          .send({ success: false, message: "Failed to delete issue" });
+      }
+    });
+    // app.get("/issues/:email", async (req, res) => {
+    //   const email = req.params.email;
+    //   console.log(email);
+    //   const query = { userEmail: email };
+    //   const result = await IssuesCollection.find(query).toArray();
+    //   res.send(result);
+    // });
 
     app.post("/report-issue", async (req, res) => {
       const newIssue = req.body;
@@ -48,6 +96,82 @@ async function run() {
         res.send(result);
         // console.log(newIssue);
       }
+    });
+
+    //STRIPE PAYMENT CHECKOUT SESSION
+    app.post("/create-checkout-session", async (req, res) => {
+      const paymentIfo = req.body;
+      const session = await stripe.checkout.sessions.create({
+        line_items: [
+          {
+            price_data: {
+              currency: "usd",
+              unit_amount: 10000,
+              product_data: {
+                name: paymentIfo.tittle,
+                description: paymentIfo.description,
+                images: [paymentIfo.image],
+              },
+            },
+            quantity: 1,
+          },
+        ],
+        mode: "payment",
+        metadata: {
+          id: paymentIfo.id,
+          location: paymentIfo.location,
+        },
+        customer_email: paymentIfo.email,
+        cancel_url: `${process.env.CLIENT_DOMAIN}/issues/${paymentIfo.id}`,
+        success_url: `${process.env.CLIENT_DOMAIN}/payment/success?session_id={CHECKOUT_SESSION_ID}`,
+      });
+      res.send({ url: session.url });
+      // res.redirect(303, session.url);
+    });
+
+    app.post("/payment/success", async (req, res) => {
+      const { sessionId } = req.body;
+      // console.log(sessionId);
+      const session = await stripe.checkout.sessions.retrieve(sessionId);
+      // console.log(session);
+      const boostAlreadyExist = await PaymentsCollection.findOne({
+        transectionId: session.payment_intent,
+      });
+      if (session.status === "complete" && !boostAlreadyExist) {
+        //save order data in db
+        const boostInfo = {
+          issueId: session.metadata.id,
+          transectionId: session.payment_intent,
+          paidBy: session.customer_email,
+          amount: session.amount_total / 100,
+        };
+        console.log(boostInfo);
+        const result = await PaymentsCollection.insertOne(boostInfo);
+        //Update Boost Status
+        await IssuesCollection.updateOne(
+          {
+            _id: new ObjectId(session.metadata.id),
+          },
+          {
+            $set: {
+              priority: "High",
+              boosted: true,
+              boostPaidBy: session.customer_email,
+            },
+            $push: {
+              timeline: {
+                status: "boosted",
+                message: "Issue boosted by user",
+                createdAt: new Date(),
+                updatedBy: session.customer_email,
+              },
+            },
+          }
+        );
+        return res.send({ boostInfo, orderId: result.insertedId });
+      }
+      console.log(boostAlreadyExist);
+      res.send({ boostAlreadyExist });
     });
 
     // Connect the client to the server	(optional starting in v4.7)
